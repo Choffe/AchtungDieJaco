89ef6003f8a3971113df5acdea659c197666bd29
Kristofer Jacobson <kristofer.jacobson@gmail.com>
2012-02-04 18:31:02 +0100

    Main och gui uppe

--- src/Gui.java
+++ src/Gui.java
@@ -1,4 +1,30 @@
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Gui {
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+
+public class Gui extends JPanel implements ActionListener{
+
+	private final int refreshRate = 140;
+	public Gui (){
+		setBackground(Color.black);
+		
+		initGame();
+	}
+	
+	public void initGame() {
+		Timer timer = new Timer(refreshRate,this);
+		timer.start();
+		
+	}
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		setBackground(Color.blue);
+		
+	}
+	
 
 }
